da0eedfb1f27e0a1763c6539e91788b0eadf31b4
khalil hadji <khalil.hadji@dataimpact.io>
2024-10-03 18:23:23 +0200

    initial dotfiles setup

---
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,31 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      -- python stuff
+      "pyright",
+      "ruff",
+      "black",
+      -- golang stuff
+      "gopls",
+      "iferr",
+      "goimports",
+      "golines",
+      "templ",
+      -- javascript
+      "typescript-language-server",
+      "eslint-lsp",
+      -- other stuff
+      "clangd",
+      "htmx-lsp",
+      -- "sqlfluff",
+      "sql-formatter",
+      "dockerfile-language-server",
+      "docker-compose-language-service",
+      "prettierd",
+      "shellcheck",
+      "shfmt",
+      "stylua",
+    },
+  },
+}
