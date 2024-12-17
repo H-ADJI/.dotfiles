050ca1564b7543931c16c3f5535adfe35981aa37
khalil hadji <h-adji_tech@proton.me>
2024-12-17 17:54:33 +0100

    change some nv configs

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,12 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
   { "tomasiser/vim-code-dark" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   -- Configure LazyVim to load gruvbox
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "gruvbox",
-  --   },
-  -- }
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  }
 }
