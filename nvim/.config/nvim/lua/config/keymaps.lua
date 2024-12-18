b9bf99344ebed7073b7241e70f5833f08c049589
khalil hadji <h-adji_tech@proton.me>
2024-12-18 22:43:30 +0100

    add buffer path keymap

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,4 +5,10 @@ vim.keymap.set({ "n", "v", "i", "x" }, "<left>", '<cmd>echo "Use vim motion !"<C
 vim.keymap.set({ "n", "v", "i", "x" }, "<right>", '<cmd>echo "Use vim motion !"<CR>')
 vim.keymap.set({ "n", "v", "i", "x" }, "<up>", '<cmd>echo "Use vim motion !"<CR>')
 vim.keymap.set({ "n", "v", "i", "x" }, "<down>", '<cmd>echo "Use vim motion !"<CR>')
-vim.keymap.set({ "v", "x" }, "<leader>cn", ":CarbonNow<CR>", { silent = true })
+vim.keymap.set({ "v", "x" }, "<leader>cn", ":CarbonNow<CR>", { desc = "Capture Code snippet", silent = true })
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>Fp",
+  ':let @+ = expand("%:p")<CR>:echo expand("%:p")<CR>',
+  { noremap = true, silent = true, desc = "Copy Buffer Full Path" }
+)
