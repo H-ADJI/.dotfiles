c48650fdcfb10fbe3b58f44f1a14b11c910eb9f6
khalil hadji <h-adji_tech@proton.me>
2024-12-28 11:32:26 +0100

    add some keybinds and remove autostart

--- zsh/zsh/path.sh
+++ zsh/zsh/path.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
-PATH="$PATH:~/.local/bin"
-PATH="$PATH:~/go/bin"
-PATH="$PATH:~/.cargo/bin"
-PATH="$PATH:~/apps"
-PATH="$PATH:~/.tmux/plugins/tmuxifier/bin/"
-PATH="$PATH:~/.spicetify"
+PATH=$PATH:~/.local/bin
+PATH=$PATH:~/go/bin
+PATH=$PATH:~/.cargo/bin
+PATH=$PATH:~/apps
+PATH=$PATH:~/.tmux/plugins/tmuxifier/bin
+PATH=$PATH:~/.spicetify
