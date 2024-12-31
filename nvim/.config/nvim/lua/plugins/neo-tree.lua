fbbe8b36b9dbce54341c6457d1bd123b2200f223
khalil hadji <h-adji_tech@proton.me>
2024-12-31 16:51:05 +0100

    fix neo tree

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -2,6 +2,6 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     keys = {
       { "<leader>e", ":Neotree focus filesystem float toggle<CR>", desc = "Neotree file explorer" },
-      { "<leader>ge", ":Neotree float git_status<CR>", desc = "Neotree git status explorer" },
+      { "<leader>ge", ":Neotree float git_status toggle<CR>", desc = "Neotree git status explorer" },
     },
   }
