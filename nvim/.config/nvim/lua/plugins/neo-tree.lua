5697e0c24a0ee09a504dc1b090865ee9beb2732a
khalil hadji <h-adji_tech@proton.me>
2025-01-01 08:56:26 +0100

    fix neo-tree commands

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,7 +1,28 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    keys = {
-      { "<leader>e", ":Neotree focus filesystem float toggle<CR>", desc = "Neotree file explorer" },
-      { "<leader>ge", ":Neotree float git_status toggle<CR>", desc = "Neotree git status explorer" },
+  "nvim-neo-tree/neo-tree.nvim",
+  keys = {
+    {
+      "<leader>fe", -- aliased to <leader>e
+      function()
+        require("neo-tree.command").execute({ toggle = true, position = "float", dir = LazyVim.root() })
+      end,
+      desc = "Explorer NeoTree (Root Dir)",
     },
-  }
+    {
+      "<leader>ge",
+      function()
+        require("neo-tree.command").execute({ source = "git_status", tposition = "float", toggle = true })
+      end,
+      desc = "Git Explorer",
+    },
+    {
+      "<leader>be",
+      function()
+        require("neo-tree.command").execute({ source = "buffers", position = "float", toggle = true })
+      end,
+      desc = "Buffer Explorer",
+    },
+    -- { "<leader>e", ":Neotree focus filesystem float toggle<CR>", desc = "Neotree file explorer" },
+    -- { "<leader>ge", ":Neotree float git_status toggle<CR>", desc = "Neotree git status explorer" },
+  },
+}
