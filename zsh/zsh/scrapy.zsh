e1dac70fa9c7967bd36680179ea4b10c21869fc5
khalil hadji <h-adji_tech@proton.me>
2025-01-04 00:22:41 +0100

    add cache to scrapy fzf

--- zsh/zsh/scrapy.zsh
+++ zsh/zsh/scrapy.zsh
@@ -2,8 +2,15 @@
 # for scrapy fuzzy search
 scrapy_fzf_spiders() {
   # Run FZF with a custom appearance
+  touch /tmp/spiders.tmp
+  if command -v scrapy &>/dev/null; then
+    scrapy list 2>/dev/null 1>/tmp/spiders.tmp
+  else
+    print -P "\nCurrent directory is not a %B%F{green}scrapy%b%f project directory, using cached spider list"
+  fi
+  zle redisplay
   local spider=$(
-    scrapy list 2>/dev/null | fzf --height 40% --layout=reverse \
+    cat /tmp/spiders.tmp | fzf --height 40% --layout=reverse \
       --border --prompt="Select Spider: " --pointer="▶ " --marker="✔ " \
       --preview="echo 'Spider: {}'" --preview-window=down:1:wrap
   )
