03cf41134f407529cc16cc064e1e0b265383fe03
khalil hadji <h-adji_tech@proton.me>
2025-01-13 18:18:56 +0100

    add lspconfig

--- nvim/.config/nvimraw/lua/plugins/neo-tree.lua
+++ nvim/.config/nvimraw/lua/plugins/neo-tree.lua
@@ -6,7 +6,6 @@ return {
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 		"s1n7ax/nvim-window-picker",
-		"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
 	keys = {
 		{ "<leader>e", ":Neotree focus filesystem float toggle<CR>", desc = "Neotree file explorer" },
@@ -34,7 +33,7 @@ return {
 						end,
 						desc = "Copy Path to Clipboard",
 					},
-					["P"] = { "toggle_preview", config = {use_image_nvim=true, use_float = true } },
+					["P"] = { "toggle_preview", config = { use_float = true } },
 				},
 			},
 		})
