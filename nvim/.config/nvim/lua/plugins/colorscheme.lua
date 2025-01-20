aefcbaddeaf61e840c58731fcac3350c6ad5f03d
khalil hadji <h-adji_tech@proton.me>
2025-01-20 15:39:21 +0100

    kitty, nvim and tmux transparency fix

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
   { "tomasiser/vim-code-dark" },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "catppuccin/nvim", opts = { transparent_background = true }, name = "catppuccin", priority = 1000 },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin",
     },
-  }
+  },
 }
