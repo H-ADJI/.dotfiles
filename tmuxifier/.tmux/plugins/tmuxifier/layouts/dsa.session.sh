3ff99a71e47edf42cc4b1e07479aeeaa88ec59de
khalil hadji <h-adji_tech@proton.me>
2025-02-13 22:31:30 +0100

    fix dsa stuff

--- tmuxifier/.tmux/plugins/tmuxifier/layouts/dsa.session.sh
+++ tmuxifier/.tmux/plugins/tmuxifier/layouts/dsa.session.sh
@@ -5,8 +5,7 @@ session_root "~/neurogenesis/"
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
 if initialize_session "dsa"; then
-  new_window "editor"
-  run_cmd "nv"
+  new_window 
 fi
 
 # Finalize session creation and switch/attach to it.
