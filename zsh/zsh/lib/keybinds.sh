f2286c142c984f94891ade628fa5bb7d43d9e839
khalil hadji <h-adji_tech@proton.me>
2025-03-17 13:37:01 +0100

    add fzf-git and suggestion and completion plugins and some keybinds

---
+++ zsh/zsh/lib/keybinds.sh
@@ -0,0 +1,9 @@
+# # emacs keybinds
+bindkey -v
+
+# accept zsh-autosuggestions
+bindkey '^o' autosuggest-accept
+bindkey '^ ' autosuggest-execute
+
+bindkey '^p' history-search-backward
+bindkey '^n' history-search-forward
---
+++ zsh/zsh/zinit/snippets.sh
@@ -0,0 +1,2 @@
+
+zinit snippet "https://raw.githubusercontent.com/junegunn/fzf-git.sh/refs/heads/main/fzf-git.sh"
