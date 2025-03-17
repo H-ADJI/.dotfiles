8b4d96a10578f44bd91b123d121e0488acb0fb10
khalil hadji <h-adji_tech@proton.me>
2025-03-17 11:57:28 +0100

    change dir layout

--- zsh/zsh/main.sh
+++ zsh/zsh/main.sh
@@ -1,30 +1,30 @@
 # starship prompt customization (should be first)
 # shellcheck disable=all
-source ~/zsh/starship.sh
+source ~/zsh/lib/starship.sh
 source ~/zsh/zinit/init.sh
-source ~/zsh/aliases.sh
-source ~/zsh/android.sh
-source ~/zsh/bat.sh
-source ~/zsh/completions.sh
-source ~/zsh/direnv.sh
-source ~/zsh/eza.sh
-source ~/zsh/fzf.sh
-source ~/zsh/git.sh
-source ~/zsh/go.sh
-source ~/zsh/kitty.sh
-source ~/zsh/leetcode.sh
-source ~/zsh/nala.sh
-source ~/zsh/npm.sh
-source ~/zsh/nvim.sh
-source ~/zsh/path.sh
-source ~/zsh/python.sh
-source ~/zsh/rust.sh
-source ~/zsh/scrapy.sh
-source ~/zsh/tmuxifier.sh
-source ~/zsh/utils.sh
-source ~/zsh/uv.sh
-source ~/zsh/zoxide.sh
-source ~/zsh/zsh.sh
+source ~/zsh/lib/aliases.sh
+source ~/zsh/lib/android.sh
+source ~/zsh/lib/bat.sh
+source ~/zsh/lib/completions.sh
+source ~/zsh/lib/direnv.sh
+source ~/zsh/lib/eza.sh
+source ~/zsh/lib/fzf.sh
+source ~/zsh/lib/git.sh
+source ~/zsh/lib/go.sh
+source ~/zsh/lib/kitty.sh
+source ~/zsh/lib/leetcode.sh
+source ~/zsh/lib/nala.sh
+source ~/zsh/lib/npm.sh
+source ~/zsh/lib/nvim.sh
+source ~/zsh/lib/path.sh
+source ~/zsh/lib/python.sh
+source ~/zsh/lib/rust.sh
+source ~/zsh/lib/scrapy.sh
+source ~/zsh/lib/tmuxifier.sh
+source ~/zsh/lib/utils.sh
+source ~/zsh/lib/uv.sh
+source ~/zsh/lib/zoxide.sh
+source ~/zsh/lib/zsh.sh
 # # emacs keybinds
 # bindkey -e
 # bindkey '^p' history-search-backward
