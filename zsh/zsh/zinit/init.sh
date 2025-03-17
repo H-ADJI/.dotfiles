fb8b40d52bb5006b174623b36c01caebbbe23eb8
khalil hadji <h-adji_tech@proton.me>
2025-03-17 13:44:16 +0100

    move zinit snippets

--- zsh/zsh/zinit/init.sh
+++ zsh/zsh/zinit/init.sh
@@ -1,4 +1,5 @@
 # zinit installation
+# shellcheck disable=all
 ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 [ ! -d "$ZINIT_HOME" ] && mkdir -p "$(dirname "$ZINIT_HOME")"
 [ ! -d "$ZINIT_HOME"/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
@@ -7,5 +8,4 @@ source "${ZINIT_HOME}/zinit.zsh"
 # plugins
 # source ~/zsh/zinit/options/zoxide.zsh
 source ~/zsh/zinit/plugins.sh
-source ~/zsh/zinit/omz-plugins.sh
 source ~/zsh/zinit/snippets.sh
--- zsh/zsh/zinit/snippets.sh
+++ zsh/zsh/zinit/snippets.sh
@@ -1,2 +1,48 @@
+# add git function library https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/git
+zinit snippet OMZL::git.zsh
 
-zinit snippet "https://raw.githubusercontent.com/junegunn/fzf-git.sh/refs/heads/main/fzf-git.sh"
+# usefull git aliases and function https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/git
+zinit snippet OMZP::git
+
+# run current or previous commands with sudo https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/sudo
+zinit snippet OMZP::sudo
+
+# replace ls with eza and its aliases https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/eza
+zinit snippet OMZP::eza
+
+# enable fuzzy completions and key binds https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/fzf
+zinit snippet OMZP::fzf
+
+# gh CLI completions https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/gh
+zinit snippet OMZP::gh
+
+# hooks direnv to terminal https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/direnv
+zinit snippet OMZP::direnv
+
+# kitty aliases and functions https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/kitty
+zinit snippet OMZP::kitty
+
+# jira CLI https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/jira
+zinit snippet OMZP::jira
+
+# rust completions for rustup cargo and rustc https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/rust
+zinit snippet OMZP::rust
+
+# npm completions and aliases I https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/npm
+zinit snippet OMZP::npm
+
+# node docs https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/node
+zinit snippet "OMZP::node"
+
+# suggest package to be installed if command not found
+zinit snippet OMZP::command-not-found
+
+# tmux aliases and settings https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/tmux
+zinit snippet OMZP::tmux
+
+# TODO: lookup how completions work
+# uv aliases and completions https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/uv
+zinit snippet OMZP::uv
+# docker aliases and completions https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/docker
+zinit snippet OMZP::docker
+zinit snippet OMZP::docker-compose
