aa1e86296f765da679d4910563a7e8c1aa796935
khalil hadji <h-adji_tech@proton.me>
2025-03-21 09:36:33 +0100

    change white color and transparency alacritty

--- zsh/zsh/zinit/plugins.sh
+++ zsh/zsh/zinit/plugins.sh
@@ -1,6 +1,8 @@
 # zinit plugins
 # zinit light https://github.com/zsh-users/zsh-history-substring-search
 zinit light "zsh-users/zsh-syntax-highlighting"
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=white'
+
 zinit light "zsh-users/zsh-autosuggestions"
 zinit light "zsh-users/zsh-completions"
 zinit light "mroth/evalcache"
