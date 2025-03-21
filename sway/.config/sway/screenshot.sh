4f263d3cf5bf060061b0205ef73b69d73a479183
khalil hadji <h-adji_tech@proton.me>
2025-03-21 12:11:36 +0100

    move volume controls to pipewire and add audio controls + screenshot saving

--- sway/.config/sway/screenshot.sh
+++ sway/.config/sway/screenshot.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+# Create the directory if it doesn't exist
+SCREENSHOT_DIR="$HOME/Pictures/Screenshots"
+mkdir -p "$SCREENSHOT_DIR"
 
+# Generate a filename based on the current date and time
+FILENAME="$SCREENSHOT_DIR/screenshot-$(date +'%Y-%m-%d_%H-%M-%S').png"
 case $1 in
-    screen) grim - | wl-copy ;;
-    region) grim -g "$(slurp)" - | wl-copy ;;
+    screen)
+        grim - | tee "$FILENAME" | wl-copy
+        ;;
+    region)
+        grim -g "$(slurp)" - | tee "$FILENAME" | wl-copy
+        ;;
 esac
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,1 +1,5 @@
 source ~/zsh/main.sh
+
+# fpath+=~/.zfunc; autoload -Uz compinit; compinit
+
+# zstyle ':completion:*' menu select
