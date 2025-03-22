1fd92d5b35c834af464e1b6596d1cbae68034358
khalil hadji <h-adji_tech@proton.me>
2025-03-22 13:05:13 +0100

    add wofi power-menu and add screenshot notif

--- sway/.config/sway/screenshot.sh
+++ sway/.config/sway/screenshot.sh
@@ -2,14 +2,17 @@
 # Create the directory if it doesn't exist
 SCREENSHOT_DIR="$HOME/Pictures/Screenshots"
 mkdir -p "$SCREENSHOT_DIR"
-
+timeout=5000
 # Generate a filename based on the current date and time
 FILENAME="$SCREENSHOT_DIR/screenshot-$(date +'%Y-%m-%d_%H-%M-%S').png"
+APP_NAME="Sway Screenshot"
 case $1 in
     screen)
         grim - | tee "$FILENAME" | wl-copy
+        notify-send "Screenshot taken" "Saved to $FILENAME" -t "$timeout" -i "$FILENAME" -a "$APP_NAME"
         ;;
     region)
         grim -g "$(slurp)" - | tee "$FILENAME" | wl-copy
+        notify-send "Screenshot taken" "Saved to $FILENAME" -t "$timeout" -i "$FILENAME" -a "$APP_NAME"
         ;;
 esac
---
+++ sway/.config/sway/wofi-power-menu.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Define menu options (keys) and their corresponding actions
+keys=(
+    $'\uf023   Log Out'
+    $'\uf186   Suspend'
+    $'\uf2dc   Hibernate'
+    $'\uf021   Reboot'
+    $'\uf011   Shutdown'
+)
+
+actions=(
+    "swaymsg exit"
+    "systemctl suspend"
+    "systemctl hibernate"
+    "systemctl reboot"
+    "systemctl poweroff"
+)
+
+# Combine the keys into a newline-separated string for the menu
+options=$(printf "%s\n" "${keys[@]}")
+
+# Launch wofi with the given options and capture the selected menu item
+choice=$(echo -e "$options" | wofi -d -i -p 'Power Menu' -W 100 -H 175 -k /dev/null)
+
+# Find the index of the selected choice in the keys array
+index=-1
+for i in "${!keys[@]}"; do
+    if [[ "${keys[$i]}" == "$choice" ]]; then
+        index=$i
+        break
+    fi
+done
+
+# If a valid option was selected, execute its corresponding action
+if [ "$index" -ne -1 ]; then
+    ${actions[$index]}
+fi
