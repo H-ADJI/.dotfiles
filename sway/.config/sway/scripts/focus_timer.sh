ed87c781c3a57375adcce11b63c440aacab81512
khalil hadji <h-adji_tech@proton.me>
2025-04-07 14:32:40 +0200

    refactor timer

--- sway/.config/sway/scripts/focus_timer.sh
+++ sway/.config/sway/scripts/focus_timer.sh
@@ -3,6 +3,8 @@
 DURATION=5400
 timeout=8000
 APP_NAME="Timer"
+STEPS=30
+BREAK=60
 
 hours=$((DURATION / 3600))
 minutes=$(((DURATION % 3600) / 60)) # Get remaining minutes
@@ -11,19 +13,20 @@ minutes=$(((DURATION % 3600) / 60)) # Get remaining minutes
 hours=$(printf "%02d" $hours)
 minutes=$(printf "%02d" $minutes)
 
-notify-send "Timer Activated" "Your $hours:$minutes of focused work has started !" -t "$timeout" -a "$APP_NAME"
-paplay /usr/share/sounds/freedesktop/stereo/complete.oga
-steps=1
+notify() {
+    notify-send "$1" "$2" -t "$timeout" -a "$APP_NAME"
+    paplay /usr/share/sounds/freedesktop/stereo/complete.oga
+}
+notify "Timer Activated" "Your $hours:$minutes of focused work has started !"
 
 while [ $DURATION -ge 0 ]; do
     if [ "$DURATION" -eq $((DURATION / 3)) ] || [ "$DURATION" -eq $((2 * DURATION / 3)) ]; then
-        notify-send "Short Break" "Take a quick 1-minute break ✨" -t "$timeout" -a "$APP_NAME"
-        paplay /usr/share/sounds/freedesktop/stereo/complete.oga
-        sleep 60
+        notify "Short Break" "Take a quick 1-minute break ✨"
+        sleep $BREAK
+        notify "Short Break" "Done"
     fi
-    sleep $steps
-    DURATION=$((DURATION - steps))
+    sleep $STEPS
+    DURATION=$((DURATION - STEPS))
 done
 
-notify-send "Time is up" "Time to rest your eyes and take a walk!" -t "$timeout" -a "$APP_NAME"
-paplay /usr/share/sounds/freedesktop/stereo/complete.oga
+notify "Time is up" "Time to rest your eyes and take a walk!"
